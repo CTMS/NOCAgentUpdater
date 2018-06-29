67648ec7cc22f6aab475ca034076b5e3fcd02199
mjimerson <maria.jimerson88@gmail.com>
2018-06-29 13:37:11 -0400

    updated for no path

--- NOCAgentUpdater.ps1
+++ NOCAgentUpdater.ps1
@@ -1,4 +1,4 @@
 Set-Location -Path C:\ctms\git_repos\NOCAgentUpdater
-git.exe pull
+start-process -filepath "C:\Program Files\Git\cmd\git.exe" -ArgumentList "pull"
 Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\InstallAgent.ini" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDOMAIN\scripts\Agent" -Force
 Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\SOAGENTSETUP.exe" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDOMAIN\scripts\Agent" -Force
