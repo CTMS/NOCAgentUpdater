f760fa0883721ef6bd0dac49350dd2c263489cbf
mjimerson <maria.jimerson88@gmail.com>
2018-06-29 13:50:55 -0400

    fixed issues with no profile

--- NOCAgentUpdater.ps1
+++ NOCAgentUpdater.ps1
@@ -1,4 +1,5 @@
+$domain = (Get-WmiObject Win32_ComputerSystem).Domain
 Set-Location -Path C:\ctms\git_repos\NOCAgentUpdater
 start-process -filepath "C:\Program Files\Git\cmd\git.exe" -ArgumentList "pull"
-Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\InstallAgent.ini" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDOMAIN\scripts\Agent" -Force
-Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\SOAGENTSETUP.exe" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDOMAIN\scripts\Agent" -Force
+Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\InstallAgent.ini" -Destination "C:\Windows\SYSVOL\sysvol\$domain\scripts\Agent" -Force
+Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\SOAGENTSETUP.exe" -Destination "C:\Windows\SYSVOL\sysvol\$domain\scripts\Agent" -Force
