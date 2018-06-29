81c9db11527e7664ee8e6c1c0f6fa0418cfaacb2
mjimerson <maria.jimerson88@gmail.com>
2018-06-29 15:25:22 -0400

    Updated script to allow for DFSR sysvol folders

--- NOCAgentUpdater.ps1
+++ NOCAgentUpdater.ps1
@@ -1,5 +1,16 @@
-$domain = (Get-WmiObject Win32_ComputerSystem).Domain
 Set-Location -Path C:\ctms\git_repos\NOCAgentUpdater
 start-process -filepath "C:\Program Files\Git\cmd\git.exe" -ArgumentList "pull"
-Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\InstallAgent.ini" -Destination "C:\Windows\SYSVOL\sysvol\$domain\scripts\Agent" -Force
-Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\SOAGENTSETUP.exe" -Destination "C:\Windows\SYSVOL\sysvol\$domain\scripts\Agent" -Force
+
+$domain = (Get-WmiObject Win32_ComputerSystem).Domain
+$AgentBin = "C:\ctms\git_repos\NOCAgentUpdater\SOAGENTSETUP.exe"
+$Agentini = "C:\ctms\git_repos\NOCAgentUpdater\InstallAgent.ini"
+
+if (Test-Path -Path C:\Windows\SYSVOL_DFSR) {
+    $Sysvol_Path = "C:\Windows\SYSVOL_DFSR\sysvol\$domain\scripts\Agent"
+}
+else {
+    $Sysvol_Path = "C:\Windows\SYSVOL\sysvol\$domain\scripts\Agent"
+}
+
+Copy-Item $AgentBin -Destination $Sysvol_Path -Force
+Copy-Item $Agentini -Destination $Sysvol_Path -Force
