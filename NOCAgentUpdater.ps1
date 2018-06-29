53a8af9815400d4b67a097c074af2ef3c53102a3
mjimerson <maria.jimerson88@gmail.com>
2018-06-29 13:06:50 -0400

    updated to only copy needed files

--- NOCAgentUpdater.ps1
+++ NOCAgentUpdater.ps1
@@ -1,3 +1,4 @@
 Set-Location -Path C:\ctms\git_repos\NOCAgentUpdater
 git.exe pull
-Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\*" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDOMAIN\scripts\Agent" -Force
+Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\InstallAgent.ini" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDOMAIN\scripts\Agent" -Force
+Copy-Item "C:\ctms\git_repos\NOCAgentUpdater\SOAGENTSETUP.exe" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDOMAIN\scripts\Agent" -Force
